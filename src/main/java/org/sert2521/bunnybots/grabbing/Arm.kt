11bc4f60e58e712f77defe167c4da7279df5dceb
Evan <evanmyersflow@gmail.com>
2017-11-12 14:41:03 -0800

    Arm class

--- src/main/java/org/sert2521/bunnybots/grabbing/Arm.kt
+++ src/main/java/org/sert2521/bunnybots/grabbing/Arm.kt
@@ -1,2 +1,14 @@
 package org.sert2521.bunnybots.grabbing
 
+import edu.wpi.first.smartdashboard.gui.elements.Command
+
+class Arm(
+        private val arm: Arm
+) : Command(grab) {
+    override fun execute(): Boolean {
+        grab(trigger.read())
+        return false
+    }
+
+    override fun end() = grab.stop()
+}
