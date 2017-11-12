355cdf1170c902a47342411c7947b16a4037b057
Aidan Heller <asfheller@gmail.com>
2017-11-12 14:14:21 -0800

    Add constants for robot arms

--- src/main/java/org/sert2521/bunnybots/util/Constants.kt
+++ src/main/java/org/sert2521/bunnybots/util/Constants.kt
@@ -9,3 +9,5 @@ const val RIGHT_FRONT_MOTOR = 10
 const val RIGHT_REAR_MOTOR = 11
 const val LEFT_FRONT_MOTOR = 12
 const val LEFT_REAR_MOTOR = 13
+const val RIGHT_ARM_MOTOR = 14
+const val LEFT_ARM_MOTOR = 15
