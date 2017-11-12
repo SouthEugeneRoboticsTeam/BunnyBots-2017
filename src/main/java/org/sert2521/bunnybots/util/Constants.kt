6676aab96375e0e375af000561802c934119bb2a
Aidan Heller <asfheller@gmail.com>
2017-11-12 14:11:18 -0800

    Rename drivetrain motor ports base on side

--- src/main/java/org/sert2521/bunnybots/util/Constants.kt
+++ src/main/java/org/sert2521/bunnybots/util/Constants.kt
@@ -5,7 +5,7 @@ const val LEFT_STICK_PORT = 0
 const val RIGHT_STICK_PORT = 1
 
 // Talon IDs
-const val FRONT_RIGHT_MOTOR = 10
-const val FRONT_LEFT_MOTOR = 12
-const val REAR_RIGHT_MOTOR = 11
-const val REAR_LEFT_MOTOR = 13
+const val RIGHT_FRONT_MOTOR = 10
+const val RIGHT_REAR_MOTOR = 11
+const val LEFT_FRONT_MOTOR = 12
+const val LEFT_REAR_MOTOR = 13
