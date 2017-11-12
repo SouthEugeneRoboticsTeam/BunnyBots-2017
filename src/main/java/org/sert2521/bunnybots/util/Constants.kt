8c07901add924a2746bad7ada81527f1cd202c8c
Aidan Heller <asfheller@gmail.com>
2017-11-12 14:02:58 -0800

    Add correct motor ports for drivetrain

--- src/main/java/org/sert2521/bunnybots/util/Constants.kt
+++ src/main/java/org/sert2521/bunnybots/util/Constants.kt
@@ -6,6 +6,6 @@ const val RIGHT_STICK_PORT = 1
 
 // Talon IDs
 const val FRONT_RIGHT_MOTOR = 10
-const val FRONT_LEFT_MOTOR = 14
+const val FRONT_LEFT_MOTOR = 12
 const val REAR_RIGHT_MOTOR = 11
-const val REAR_LEFT_MOTOR = 15
+const val REAR_LEFT_MOTOR = 13
