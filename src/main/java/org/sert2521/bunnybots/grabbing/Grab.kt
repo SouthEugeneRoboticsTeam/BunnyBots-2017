5d7a447d13822dc754976b6fa06b66932b403612
Aidan Heller <asfheller@gmail.com>
2017-11-14 17:38:21 -0800

    Finish command Grab

--- src/main/java/org/sert2521/bunnybots/grabbing/Grab.kt
+++ src/main/java/org/sert2521/bunnybots/grabbing/Grab.kt
@@ -1,9 +1,15 @@
 package org.sert2521.bunnybots.grabbing
 
 import org.strongback.command.Command
+import org.strongback.components.Solenoid
 
-class Grab() : Command() {
+class Grab(
+        private val claw: Solenoid
+) : Command() {
     override fun execute(): Boolean {
+        claw.extend()
         return false
     }
+
+    override fun end() { claw.retract() }
 }
--- src/main/java/org/sert2521/bunnybots/grabbing/GrabUtils.kt
+++ src/main/java/org/sert2521/bunnybots/grabbing/GrabUtils.kt
@@ -4,20 +4,16 @@ import org.sert2521.bunnybots.grabbing.Grab
 import org.sert2521.bunnybots.util.leftJoystick
 import org.sert2521.bunnybots.util.onTriggeredLifecycleSubmit
 import org.strongback.Strongback
-import org.strongback.command.Command
 import org.strongback.components.Solenoid
 import org.strongback.hardware.Hardware
 import java.util.function.Supplier
 
 private val claw = Hardware.Solenoids.doubleSolenoid(0, 0, Solenoid.Direction.RETRACTING)
 
-
-
 fun initGrab() {
-
     Strongback.switchReactor().apply {
         onTriggeredLifecycleSubmit(leftJoystick.trigger, Supplier {
-            Grab(claw, leftJoystick.trigger)
+            Grab(claw)
         })
     }
 }
