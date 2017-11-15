d9a2203be6feda2e88cf0a40b44975a70ba993b0
Aidan Heller <asfheller@gmail.com>
2017-11-14 21:27:02 -0800

    Inline execute function

--- src/main/java/org/sert2521/bunnybots/claw/Grab.kt
+++ src/main/java/org/sert2521/bunnybots/claw/Grab.kt
@@ -4,13 +4,12 @@ import org.strongback.command.Command
 import org.strongback.components.Solenoid
 
 class Grab(private val claw: Solenoid) : Command() {
-    override fun execute(): Boolean {
-        return false
-    }
 
     override fun initialize() {
         claw.extend()
     }
+
+    override fun execute() = false
 
     override fun end() {
         claw.retract()
