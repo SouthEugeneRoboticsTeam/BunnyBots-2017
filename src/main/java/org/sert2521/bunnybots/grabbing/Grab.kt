b073d50462f46504aaafa851d2f7be2ebb8cc199
Aidan Heller <asfheller@gmail.com>
2017-11-14 17:25:14 -0800

    Remember to declare package

--- src/main/java/org/sert2521/bunnybots/grabbing/Grab.kt
+++ src/main/java/org/sert2521/bunnybots/grabbing/Grab.kt
@@ -1,3 +1,5 @@
+package org.sert2521.bunnybots.grabbing
+
 import org.strongback.command.Command
 
 class Grab() : Command() {
--- src/main/java/org/sert2521/bunnybots/grabbing/GrabUtils.kt
+++ src/main/java/org/sert2521/bunnybots/grabbing/GrabUtils.kt
@@ -1,5 +1,6 @@
 package org.sert2521.bunnybots.driving
 
+import org.sert2521.bunnybots.grabbing.Grab
 import org.sert2521.bunnybots.util.leftJoystick
 import org.sert2521.bunnybots.util.onTriggeredLifecycleSubmit
 import org.strongback.Strongback
@@ -8,13 +9,15 @@ import org.strongback.components.Solenoid
 import org.strongback.hardware.Hardware
 import java.util.function.Supplier
 
-
 private val claw = Hardware.Solenoids.doubleSolenoid(0, 0, Solenoid.Direction.RETRACTING)
 
+
+
 fun initGrab() {
+
     Strongback.switchReactor().apply {
         onTriggeredLifecycleSubmit(leftJoystick.trigger, Supplier {
-            Grab(grab, leftJoystick.trigger)
+            Grab(claw, leftJoystick.trigger)
         })
     }
 }
