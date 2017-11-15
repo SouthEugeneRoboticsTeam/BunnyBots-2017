84c0adf502a8df32920c55ea302698b2a79b56f5
Aidan Heller <asfheller@gmail.com>
2017-11-14 21:22:37 -0800

    Move claw.extend call into initialize function

--- src/main/java/org/sert2521/bunnybots/claw/Grab.kt
+++ src/main/java/org/sert2521/bunnybots/claw/Grab.kt
@@ -5,8 +5,11 @@ import org.strongback.components.Solenoid
 
 class Grab(private val claw: Solenoid) : Command() {
     override fun execute(): Boolean {
-        claw.extend()
         return false
+    }
+
+    override fun initialize() {
+        claw.extend()
     }
 
     override fun end() {
