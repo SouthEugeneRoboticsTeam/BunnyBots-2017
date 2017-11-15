2736209654d25873fb2a6f77842811ce73312385
Evan <evanmyersflow@gmail.com>
2017-11-14 16:49:38 -0800

    defaultGrab

--- src/main/java/org/sert2521/bunnybots/grabbing/GrabUtils.kt
+++ src/main/java/org/sert2521/bunnybots/grabbing/GrabUtils.kt
@@ -1,7 +1,16 @@
 package org.sert2521.bunnybots.driving
 
+import org.sert2521.bunnybots.util.leftJoystick
+import org.strongback.command.Command
 import org.strongback.components.Solenoid
 import org.strongback.hardware.Hardware
 
 
 private val grab = Hardware.Solenoids.doubleSolenoid(0, 0, Solenoid.Direction.RETRACTING)
+
+private val defaultGrab: Command
+    get() = Grab(grab, leftJoystick.trigger)
+
+
+
+// onTriggeredLifeCycle
