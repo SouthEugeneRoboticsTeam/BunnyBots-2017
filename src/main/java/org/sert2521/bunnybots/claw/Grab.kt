40e43e11f26a29c6b8949864a3a59dd2bad6c759
Aidan Heller <asfheller@gmail.com>
2017-11-14 20:49:57 -0800

    Inline parameters in Grab command

--- src/main/java/org/sert2521/bunnybots/claw/Grab.kt
+++ src/main/java/org/sert2521/bunnybots/claw/Grab.kt
@@ -3,9 +3,7 @@ package org.sert2521.bunnybots.claw
 import org.strongback.command.Command
 import org.strongback.components.Solenoid
 
-class Grab(
-        private val claw: Solenoid
-) : Command() {
+class Grab(private val claw: Solenoid) : Command() {
     override fun execute(): Boolean {
         claw.extend()
         return false
